8fc173834f03302cae891ae368e0494ad6976360
David Sherret <dsherret@users.noreply.github.com>
2025-07-25 15:32:48 -0400

    perf: make `normalize_path` faster if already normalized (#12)

---
+++ benches/bench.rs
@@ -0,0 +1,21 @@
+use std::borrow::Cow;
+use std::path::PathBuf;
+
+use deno_path_util::normalize_path;
+
+fn main() {
+  // Run registered benchmarks.
+  divan::main();
+}
+
+#[divan::bench(sample_size = 512000)]
+fn bench_normalize_path_changed(bencher: divan::Bencher) {
+  let path = PathBuf::from("/testing/../this/./out/testing/../test");
+  bencher.bench(|| normalize_path(Cow::Borrowed(&path)))
+}
+
+#[divan::bench(sample_size = 512000)]
+fn bench_normalize_path_no_change(bencher: divan::Bencher) {
+  let path = PathBuf::from("/testing/this/out/testing/test");
+  bencher.bench(|| normalize_path(Cow::Borrowed(&path)))
+}
